7614a9601245b4d6833d9dd663c2442f4b1710cf
OlenaLotos <olenameshanych@gmail.com>
2022-06-13 18:31:25 +0300

    makes fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+import PhotoApiService from './partials/api'
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const searchForm = document.querySelector('.search-form')
+const loadMoreBtn = document.querySelector('.load-more')
+ 
+const photoApiService = new PhotoApiService();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoad);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    photoApiService.query = e.currentTarget.elements.searchQuery.value;
+    photoApiService.fetchPhotos();
+    }
+
+function onLoad() {
+photoApiService.fetchPhotos();
+}
---
+++ src/partials/api.js
@@ -0,0 +1,26 @@
+export default class PhotoApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+
+    }
+    fetchPhotos() {
+        console.log(this);
+        const url = `https://pixabay.com/api/?key=28020117-3a98d2f0db4e6cde6fe7bd6ea&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+    fetch(url)
+    .then(r => r.json())
+        .then(data => {
+            this.page += 1;
+    });
+    }
+    
+    get query() {
+    return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
